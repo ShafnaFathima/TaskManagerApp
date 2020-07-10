864af56c1ad6ce00aec4c37453b303fa55cbff5e
Shafna Fathima <shafnafathima15@gmail.com>
2020-07-10 16:15:46 +0530

    Adaptive triggers in AddTaskpage

--- App.xaml.cs
+++ App.xaml.cs
@@ -20,6 +20,7 @@ using System.Security.Cryptography.X509Certificates;
 using TaskManagerApp.DB;
 using Windows.Storage;
 using System.ServiceModel.Channels;
+using TaskManagerApp.Model;
 
 namespace TaskManagerApp
 {
@@ -35,7 +36,8 @@ namespace TaskManagerApp
 
         internal static string CurrentUser;
         public static ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-
+        Object value = App.localSettings.Values["IsFirstLaunch"];
+    
         public App()
         {
             this.InitializeComponent();
@@ -49,7 +51,49 @@ namespace TaskManagerApp
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+        {       
+            if(localSettings.Values["IsFirstLaunch"] ==null||(bool)value==true)
+            {
+                
+                    localSettings.Values["IsFirstLaunch"] = false;
+                    var vault = new Windows.Security.Credentials.PasswordVault();
+                    vault.Add(new Windows.Security.Credentials.PasswordCredential("TaskManagerApp", "Sabi", "123"));
+                    vault.Add(new Windows.Security.Credentials.PasswordCredential("TaskManagerApp", "Sabitha", "123"));
+                    UserDB.AddUser("Sabi", "/Assets/avatar1.PNG");
+                    UserDB.AddUser("Sabitha", "/Assets/avatar4.PNG");
+                    TaskDB.AddTask(new Model.TaskModel() { TaskName = "Cliq Login Page", TaskId = 12233344441, 
+                        AssignedByUser = "Sabi", AssignedToUser = "Sabi", Priority = 1, StartDate = (DateTimeOffset)DateTime.Today,
+                        EndDate = (DateTimeOffset)DateTime.Today, Description = "lopoioiui wtyetyuiyruiewo" });
+                    TaskDB.AddTask(new Model.TaskModel()
+                    {
+                        TaskName = "Cliq SignIn Page",
+                        TaskId = 9876543211,
+                        AssignedByUser = "Sabitha",
+                        AssignedToUser = "Sabi",
+                        Priority = 2,
+                        StartDate = (DateTimeOffset)DateTime.Today,
+                        EndDate = (DateTimeOffset)DateTime.Today,
+                        Description = "lopoioiui wtyetyuiyruiewo wqeqtwet"
+                    });
+                    TaskDB.AddTask(new Model.TaskModel()
+                    {
+                        TaskName = "Content Writing",
+                        TaskId = 1234567891,
+                        AssignedByUser = "Sabitha",
+                        AssignedToUser = "Sabitha",
+                        Priority = 1,
+                        StartDate = (DateTimeOffset)DateTime.Today,
+                        EndDate = (DateTimeOffset)DateTime.Today,
+                        Description = "lopoioiui wtyetyuiyruiewo yuwiyeueiytiu"
+                    });
+                    CommentDB.AddComment(new Model.Comment() { AuthorName = "sabi", CommentToTaskId = 1234567891, Content = " If you cannot find a way to fit supporting evidence in just one or two sentences, use a different example altogether. There are certain topics that require a lot of room for explanation, so be careful not to choose a topic for your essay that will require too much evidence to support.",
+                                        CommentId=999886601,Date=DateTime.Now,Heart=0,Happy=0,Sad=0,Like=0 });
+                    CommentDB.AddComment(new Model.Comment() { AuthorName = "sabitha", CommentToTaskId = 1234567891, Content = "Okay Done!", CommentId = 999886611, Date = DateTime.Now, Heart = 0, Happy = 0, Sad = 0, Like = 0 });
+                    CommentDB.AddComment(new Model.Comment() { AuthorName = "sabi", CommentToTaskId = 9876543211, Content = " Only by examining how you reflect on your qualities can college admissions officers gain an understanding", CommentId = 999886621, Date = DateTime.Now, Heart = 0, Happy = 0, Sad = 0, Like = 0 });
+                    CommentDB.AddComment(new Model.Comment() { AuthorName = "sabitha", CommentToTaskId = 12233344441, Content = " Present, support, and introspect.", CommentId = 999886631, Date = DateTime.Now, Heart = 0, Happy = 0, Sad = 0, Like = 0 });            
+                
+            }
+          
 
             Frame rootFrame = Window.Current.Content as Frame;
 
@@ -78,6 +122,7 @@ namespace TaskManagerApp
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
+
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
